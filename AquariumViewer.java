b4b490c2f91ca169c5b878d2aadfacaae30af947
kcaashish <kcaashish@gmail.com>
2020-05-11 18:22:35 +0545

    added the Aquarium Viewer constructor, displayGrid and method to increase font size

--- AquariumViewer.java
+++ AquariumViewer.java
@@ -6,12 +6,13 @@
  * @version 2020
  */
 import java.awt.*;
-import java.awt.event.*; 
-import javax.swing.SwingUtilities;
+import java.awt.event.*;
+import java.time.OffsetDateTime;
+import javax.swing.*;
 
 public class AquariumViewer implements MouseListener
 {
-    private final int BOXSIZE = 40;          // the size of each square
+    private final int BOXSIZE = 50;          // the size of each square
     private final int OFFSET  = BOXSIZE * 2; // the gap around the board
     private       int WINDOWSIZE;            // set this in the constructor 
     
@@ -19,12 +20,30 @@ public class AquariumViewer implements MouseListener
     private int        size; // the puzzle is size x size
     private SimpleCanvas sc; // the display window
 
+    private final static Color BACK_COLOUR = Color.white;
+    private final static Color GRID_COLOUR = Color.black;
+
+
+
     /**
      * Main constructor for objects of class AquariumViewer.
      * Sets all fields, and displays the initial puzzle.
      */
     public AquariumViewer(Aquarium puzzle)
     {
+        this.puzzle = puzzle;
+        size = puzzle.getSize();
+
+        WINDOWSIZE = size * BOXSIZE + 2 * OFFSET;
+
+        sc = new SimpleCanvas("Aquarium Game", WINDOWSIZE, WINDOWSIZE, BACK_COLOUR);
+        sc.addMouseListener(this);
+        fontSize(2);
+
+        displayGrid();
+        displayNumbers();
+        displayAquariums();
+        displayButtons();
         // TODO 8
     }
     
@@ -44,14 +63,23 @@ public class AquariumViewer implements MouseListener
     {
         this(61);
     }
-    
+
+    /**
+     * Increases the font size by a factor x.
+     */
+    private void fontSize(double x)
+    {
+        sc.setFont(sc.getFont()
+                .deriveFont((float) (sc.getFont().getSize() * x)));
+    }
+
     /**
      * Returns the current state of the puzzle.
      */
     public Aquarium getPuzzle()
     {
         // TODO 7a
-        return null;
+        return puzzle;
     }
     
     /**
@@ -60,7 +88,7 @@ public class AquariumViewer implements MouseListener
     public int getSize()
     {
         // TODO 7b
-        return -1;
+        return size;
     }
 
     /**
@@ -69,7 +97,7 @@ public class AquariumViewer implements MouseListener
     public SimpleCanvas getCanvas()
     {
         // TODO 7c
-        return null;
+        return sc;
     }
     
     /**
@@ -85,6 +113,17 @@ public class AquariumViewer implements MouseListener
      */
     public void displayGrid()
     {
+        for (int i = 0; i <= size; i++) {
+            int changingLinePosition = OFFSET + i * BOXSIZE;
+            int lineEnd = OFFSET + size * BOXSIZE;
+
+            //draw vertical lines
+            sc.drawLine(changingLinePosition, OFFSET, changingLinePosition, lineEnd, GRID_COLOUR);
+
+            //draw horizontal lines
+            sc.drawLine(OFFSET, changingLinePosition, lineEnd, changingLinePosition, GRID_COLOUR);
+        }
+
         // TODO 9
     }
     
